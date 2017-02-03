66d27c996be7cbf9950156f3ba49dcbcf471c68a
Khrystyna <khrystynkakitty@gmail.com>
2017-02-03 14:34:06 +0000

    text

---
+++ index.js
@@ -0,0 +1,17 @@
+var x;
+if (x == undefinden) {
+    console.log("x isn’t defined! just deal with it");
+}
+
+console.log(x);
+
+
+
+
+function bla() {
+var customer = {
+    name: "jenny"
+};
+console.log(customer.name);
+}
+bla();
