7b074419ccd491ca1be79103e3f60d759a7efa94
Khrystyna <khrystynkakitty@gmail.com>
2017-02-10 08:54:12 +0000

    text

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var x;
-if (x == undefinden) {
+if (x == undefined) {
     console.log("x isn’t defined! just deal with it");
 }
-
 console.log(x);
-
 
 
 
@@ -13,5 +11,38 @@ var customer = {
     name: "jenny"
 };
 console.log(customer.name);
+console.log(customer.phone);
 }
 bla();
+
+
+var customer = {
+    name: "jenny"
+};
+console.log(customer.adress);
+
+var tet = null;
+console.log(typeof tet);
+
+console.log(typeof undefined);
+
+var bjf = 0/0; 
+console.log(bjf);
+
+console.log(1/0);
+
+if (99 == "99") {
+    console.log("it's true");
+}
+
+
+function Duck(sound) {
+    this.sound = sound;
+    this.quack = function() {
+        console.log(this.sound);
+    };
+}
+var toy = new Duck("quack quack");
+toy.quack();
+console.log(typeof toy);
+console.log(toy instanceof Duck);
